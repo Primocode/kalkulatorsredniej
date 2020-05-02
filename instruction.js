9c0e8b2b5c03eb9dd7279d5e933c1fd4db76d908
Primocode <20przemek04@wp.pl>
2020-05-02 16:49:18 +0200

    Update instruction.js

--- instruction.js
+++ instruction.js
@@ -1,69 +1,60 @@
-
 let number = 0;
 
 if (true) {
 	const window = document.querySelector('.window')
+	window.style.display = "flex";
 	const window1 = document.querySelectorAll('.window-container')
 	const next = document.querySelector('.next')
 	const previous = document.querySelector('.previous');
 	const goto = document.querySelector('.goto');
 	window1.item(number).className = "window-container-active"
 
-
 	if (localStorage.getItem("info") == "false") {
-		window.style.display = "none";
+		window.remove();
 	}
-		
 
-	function sprawdz() {
+	function check() {
 		if (number == 0) {
 			previous.style.display = "none";
 		}
-		if (number == 3) {
+		if (number == window1.length - 1) {
 			next.style.display = "none";
 			goto.style.display= "block";
 		}
-		if (number == 4) {
+		if (number == window1.length) {
 			window.style.display = "none";
 		}
 	}
-	sprawdz()
+	check()
 
-	const nextFunction = () => {
-		sprawdz()
-		number++
-		previous.style.display = "block";
-		window1.item(0).className = "window-container"
-		window1.item(1).className = "window-container"
-		window1.item(2).className = "window-container"
-		window1.item(3).className = "window-container"
-		window1.item(number).className = "window-container-active"
-		sprawdz()
+	const addClassName = () => {
+		for (let i = 0; i < window1.length; i++) {
+			window1[i].className = "window-container";
+		}
 	}
 
+	const nextFunction = () => {
+		number++
+		previous.style.display = "block";
+		addClassName()
+		window1.item(number).className = "window-container-active"
+		check()
+	}
 	next.addEventListener('click', nextFunction);
 
 	const previousFunction = () => {
 		number--
 		goto.style.display = "none";
 		next.style.display = "block";
-		window1.item(0).className = "window-container"
-		window1.item(1).className = "window-container"
-		window1.item(2).className = "window-container"
-		window1.item(3).className = "window-container"
+		addClassName()
 		window1.item(number).className = "window-container-active"
-		sprawdz()
+		check()
 	}
-
 	previous.addEventListener('click', previousFunction);
 
 	const gotoFunction = () => {
-		window.style.display = "none";
+		window.remove();
 	}
-
 	goto.addEventListener('click', gotoFunction);
 }
 localStorage.setItem("info", "false");
-
-
-
